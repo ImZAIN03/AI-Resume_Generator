6879b1fa9eb46edc8e8b2bcf2014dc122b08e52d
Muhammed Zain <muhammedzain2256@gmail.com>
2024-06-20 22:35:00 +0500

    React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import { Outlet } from 'react-router-dom'
+import { Home } from 'lucide-react'
 
 function App() {
 
   return (
     <>
-      Zain
-      <Button>Hey</Button>
+      <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import SignIn from './auth/SignIn/SignIn.jsx'
+import Home from './home/Home';
+import Dashboard from './dashboard/Dashboard.jsx';
 
+const router = createBrowserRouter([
+  {
+    element:<App/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>
+      },
+    ]
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignIn/>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
