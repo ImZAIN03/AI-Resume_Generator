5986480d79c454c6fcb49b7a876c923f8716e44c
Muhammed Zain <muhammedzain2256@gmail.com>
2024-06-21 19:53:10 +0500

    Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import SignIn from './auth/SignIn/SignIn.jsx'
 import Home from './home/Home';
 import Dashboard from './dashboard/Dashboard.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
+import SignInPage from './auth/SignIn/SignInPage.jsx';
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const router = createBrowserRouter([
   {
     element:<App/>,
@@ -23,11 +25,13 @@ const router = createBrowserRouter([
   },
   {
     path: '/auth/sign-in',
-    element: <SignIn/>
+    element: <SignInPage/>
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <RouterProvider router={router} />
+    </ClerkProvider>    
   </React.StrictMode>,
 )
